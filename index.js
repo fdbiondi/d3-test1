61f1018ee05706fb19f83fc2faa7f12bd03582bd
Federico Biondi <fdbion@gmail.com>
2021-10-13 22:22:36 -0300

    create link line

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
-d3.json("data.json").then((data) => {
+d3.json('data.json').then((data) => {
 
   const simulation = d3.forceSimulation(data.nodes)
-    .force("charge", d3.forceManyBody().strength(-100))
-    .force("link", d3.forceLink(data.links)
+    .force('charge', d3.forceManyBody().strength(-100))
+    .force('link', d3.forceLink(data.links)
       .id(d => d.id)
       .distance(75))
-    .force("center", d3.forceCenter(300, 300))
+    .force('center', d3.forceCenter(300, 300))
 
-
-  const svg = d3.select("#target");
+  const svg = d3.select('#target');
 
   const node = svg
-    .selectAll("circle")
+    .selectAll('circle')
     .data(data.nodes)
     .enter()
-    .append("circle")
-    .attr("r", 15)
-    .attr("stroke", "green")
-    .attr("stroke-width", 0.5)
-    .style("fill", "blue")
+    .append('circle')
+    .attr('r', 15)
+    .attr('stroke', 'green')
+    .attr('stroke-width', 0.5)
+    .style('fill', 'blue')
 
   const link = svg
-    .selectAll("path.link")
+    .selectAll('path.link')
+    .data(data.links)
+    .enter()
+    .append('path')
+    .attr('stroke', 'black')
+    .attr('fill', 'none')
 
-  simulation.on("tick", () => {
+  const lineGenerator = d3.line()
+
+  simulation.on('tick', () => {
     node
-      .attr("cx", (d) => d.x)
-      .attr("cy", (d) => d.y)
+      .attr('cx', (d) => d.x)
+      .attr('cy', (d) => d.y)
+
+    link.attr('d', (d) => lineGenerator([
+      [d.source.x, d.source.y],
+      [d.target.x, d.target.y],
+    ]))
   })
 })
